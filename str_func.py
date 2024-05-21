ba60ae83a6fb98d7f3df29cb4f6d90848cf22815
Mikhail Polyakov <timadgi@gmail.com>
2024-05-21 19:37:43 +0500

    fixses funcion text_upper

--- str_func.py
+++ str_func.py
@@ -1,6 +1,3 @@
 def text_upper():
-    user_text = input("Перевод текста в заглавные буквы ")
-    print(user_text.upper())
-
-
-text_upper()
+    user_text = input("Введите текст ")
+    return user_text.upper()
